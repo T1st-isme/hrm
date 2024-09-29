951b08b97b46143568295059470b43e2344241f8
T1st <trientrung102@gmail.com>
2024-09-29 17:29:31 +0700

    test ting all function after merge

--- client/app/admin/payroll/page.tsx
+++ client/app/admin/payroll/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import MainLayout from "../../components/MainLayout";
 import withAuth from "../../components/withAuth";
+
 const PayrollPage = () => {
     return (
-        <MainLayout title="Payroll">
-            <div>Payroll</div>
+        <MainLayout title="Payroll Page">
+            <div className="container mx-auto px-4">
+                <h1 className="text-3xl font-bold mb-6">Payroll</h1>
+            </div>
         </MainLayout>
     );
 };
